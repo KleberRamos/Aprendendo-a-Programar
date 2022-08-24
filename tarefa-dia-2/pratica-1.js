5b0a36ec8a257821500e4cb9f639579df1c0346d
Kleber Ramos <thskbr2008@gmail.com>
2022-08-24 16:21:39 -0300

    Sexto commit Kleber

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -10,5 +10,4 @@ minhaProfissao = "Assistente Adm Senior"
 minhaIdade = "40"
 meuLugar = "Bahhamas"
 
-console.log (meuNome, meuEmail, minhaProfissao, minhaIdade, meuLugar)
-
+console.log (`Ola, ${meuNome}! Somos aqui da agencia de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aereas para ${meuLugar}. Confirma pra gente alguns dados? A sua idade e ${minhaIdade}, voce e ${minhaProfissao} e o seu email e ${meuEmail}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
