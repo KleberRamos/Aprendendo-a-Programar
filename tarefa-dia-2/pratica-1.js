35f57de24be111660eba23473dc8c7f2990f6ee1
Kleber Ramos <thskbr2008@gmail.com>
2022-08-24 15:27:50 -0300

    Quarto commit Kleber

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,17 @@
-let name
-let email
-let profissao
-let idade
-let lugar
+let meuNome
+let meuEmail
+let minhaProfissao
+let minhaIdade
+let meuLugar
+
+meuNome = "Kleber"
+meuEmail = "krg@hotmail.com"
+minhaProfissao = "Assistente Adm Senior"
+minhaIdade = "40"
+meuLugar = "Bahhamas"
+
+console.log (meuNome)
+console.log (meuEmail)
+console.log (minhaProfissao)
+console.log (minhaIdade)
+console.log (meuLugar)
