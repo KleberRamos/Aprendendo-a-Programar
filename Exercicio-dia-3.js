69c2dc1883e977d1644c574764fefe6d054566a3
Kleber Ramos <thskbr2008@gmail.com>
2022-08-25 12:29:28 -0300

    Oitavo commit Kleber

---
+++ Exercicio-dia-3.js
@@ -0,0 +1,11 @@
+/* 1) Site de conteúdo adulto. Imaginar que estamos trabalhando para um site de conteúdo adulto. Quando carregamos a página no navegador, verificar a idade do usuário logado.
+// Se a idade informada pelo usuário for menor que 18 anos, mostrar a mensagem "Conteúdo proibido para menores de 18 anos de idade" */
+
+let nome = "Kleber"
+let idade = 05  
+
+if (idade >= 18) {  
+    console.log ("Acesso Permitido")
+} else {
+    console.log ("Acesso Negado")
+}
