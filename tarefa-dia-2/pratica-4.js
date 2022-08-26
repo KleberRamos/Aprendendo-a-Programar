8e2b7e5408cf77f474c68ff85ec0f50932104a6b
Kleber Ramos <thskbr2008@gmail.com>
2022-08-26 15:33:05 -0300

    Kleber

--- tarefa-dia-2/pratica-4.js
+++ tarefa-dia-2/pratica-4.js
@@ -1,6 +1,17 @@
 
-let produtoA = {"nome": "camiseta", "valor": 100.00*1.2, "internacional": true};
-let produtoB = {"nome": "perfume", "valor": 200.00*1.2, "internacional": true};
-let produtoC = {"nome": "sandália", "valor": 120.00*1.12, "internacional": false};
+let produtoA = {"nome": "camiseta", "valor": 100.00, "internacional": true};
+let produtoB = {"nome": "perfume", "valor": 200.00, "internacional": true};
+let produtoC = {"nome": "sandália", "valor": 120.00, "internacional": false};
 
-console.log (produtoA + produtoB + produtoC)
+let = [produtoA, produtoB, produtoC]
+
+for (var i = 0; i < produtoA.length; produtoB.legnth, productoC.length, i++) {
+    console.log (produtoA[i])
+}
+    console.log (produtoA[i])
+
+ if (i.internacional == true) {
+    console.log(`o ${produtoA[i].valor} é internacional e seu valor com imposto é R$ + i.valor * 1.2`)
+} else { 
+    console.log(`o ${produtoA[i].internacional} é nacional e seu valor com imposto é R$ + i.valor * 1.12`)
+}
