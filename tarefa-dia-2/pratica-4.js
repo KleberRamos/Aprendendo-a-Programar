f471be1e7e6a2cc2008ae1bf1bb8de6cce5a58e6
Kleber Ramos <thskbr2008@gmail.com>
2022-08-26 12:05:06 -0300

    Décimo terceiro commit Kleber

---
+++ tarefa-dia-2/pratica-4.js
@@ -0,0 +1,6 @@
+
+let produtoA = {"nome": "camiseta", "valor": 100.00*1.2, "internacional": true};
+let produtoB = {"nome": "perfume", "valor": 200.00*1.2, "internacional": true};
+let produtoC = {"nome": "sandália", "valor": 120.00*1.12, "internacional": false};
+
+console.log (produtoA + produtoB + produtoC)
